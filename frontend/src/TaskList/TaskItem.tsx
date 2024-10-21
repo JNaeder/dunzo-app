cff31401a46b83f905323d4be1331027fdfdb58b
NaederJ <naederj@oregonstate.edu>
2024-10-20 22:47:24 -0400

    Added checkbox functionality, still needs work

--- frontend/src/TaskList/TaskItem.tsx
+++ frontend/src/TaskList/TaskItem.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import { Task } from "../types";
 import CircleIcon from "@mui/icons-material/Circle";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-export default function TaskItem({ task }: { task: Task }) {
+export default function TaskItem({
+  task,
+  taskList,
+}: {
+  task: Task;
+  taskList: Task[];
+}) {
   const [taskOpen, setTaskOpen] = useState(false);
+  const [currentTaskState, setCurrentTaskState] = useState(task.completed);
 
   return (
     <>
@@ -13,7 +21,23 @@ export default function TaskItem({ task }: { task: Task }) {
         className={taskOpen ? `task_list_item_open` : `task_list_item_closed`}
       >
         <div className="task_list_item_top_row">
-          <CircleIcon />
+          <button
+            className="task_checkbox_button"
+            onClick={() => {
+              const updatedTask = task;
+              updatedTask.completed = !task.completed;
+              const taskIndex = task.id - 1;
+              taskList.splice(taskIndex, 1);
+              taskList.splice(taskIndex, 0, updatedTask);
+              setCurrentTaskState(updatedTask.completed);
+            }}
+          >
+            {currentTaskState ? (
+              <CheckCircleIcon className="task_checkbox" />
+            ) : (
+              <CircleIcon className="task_checkbox" />
+            )}
+          </button>
           <p className="task_item_title">{task.title}</p>
           <p className={`task_list_priority_${task.priority}`}>
             {task.priority.toUpperCase()}
